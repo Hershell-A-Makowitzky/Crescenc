d516e2e1386d59c657e3dfd58927516527d9ca3e
Hershell A. Makowitzky <hershellmakowitzky@Abulafia-Max.local>
2023-06-02 18:41:53 +0200

    new stuff

--- sha.zig
+++ sha.zig
@@ -41,6 +41,16 @@ fn wnot(x: Word) Word {
     return result;
 }
 
+fn circular(n: u5, w: Word) Word {
+    var tmpA: u64 = undefined;
+    var tmpB: u64 = undefined;
+    tmpA = std.mem.bytesToValue(u64, &w) << n;
+    tmpB = std.mem.bytesToValue(u64, &w) >> (32 - n);
+
+    var result: Word = wor((), (w >> (32 - n)));
+    return result;
+}
+
 fn f(u: u8, b: Word, c: Word, d: Word) !Word {
     if (u <= 19 ) {
         return wor(wand(b, c), wand(wnot(b), d));
@@ -55,6 +65,22 @@ fn f(u: u8, b: Word, c: Word, d: Word) !Word {
         return wxor(b, wxor(c, d));
     }
     return error.WordError;
+}
+
+fn k(u: u8) !Word {
+    if (u <= 19) {
+        return K1;
+    }
+    if (u <= 39) {
+        return K2;
+    }
+    if (u <= 59) {
+        return K3;
+    }
+    if (u <= 79) {
+        return K4;
+    }
+    return error.KError;
 }
 
 fn padding(block: *Block, message: [] const u8) void {
@@ -79,6 +105,41 @@ fn padding(block: *Block, message: [] const u8) void {
     }
 }
 
+test "k" {
+    const k1 = try k(1);
+    const k2 = try k(21);
+    const k3 = try k(41);
+    const k4 = try k(61);
+    std.debug.assert(std.mem.eql(u8, &K1, &k1));
+    std.debug.assert(std.mem.eql(u8, &K2, &k2));
+    std.debug.assert(std.mem.eql(u8, &K3, &k3));
+    std.debug.assert(std.mem.eql(u8, &K4, &k4));
+}
+
+// fn calculate(message: [] const u8) !void {
+//     var block: Block = undefined;
+//     var bufferA: [5]Word = {
+//         [_]u8{ '\x67', '\x45', '\x23', '\x01' };
+//         [_]u8{ '\xEF', '\xCD', '\xAB', '\x89' };
+//         [_]u8{ '\x98', '\xBA', '\xDC', '\xFE' };
+//         [_]u8{ '\x10', '\x32', '\x54', '\x76' };
+//         [_]u8{ '\xC3', '\xD2', '\xE1', '\xF0' };
+//     };
+//     var bufferB: [5]Word = undefined;
+//     var temp: Word = undefined;
+//     var seq: [80]Word = undefined;
+
+//     padding(&block, message);
+
+//     for (block) |val, i| {
+//         seq[i] = val;
+//     }
+
+//     for ([_]u8{} ** 64) |_, i| {
+//         seq[i + 16] =
+//     }
+// }
+
 pub fn main() !void {
     const a: Word = [_]u8 {'\x61', '\x62', '\x63', '\x64'};
     const b: Word = [_]u8 {'\x65', '\x66', '\x67', '\x68'};
@@ -102,6 +163,15 @@ pub fn main() !void {
     // check(message);
     var block: Block = undefined;
     padding(&block, message);
-
+    // const msg = try k(40);
+    // std.debug.print("\n", .{});
+    // for (msg) |val| {
+    //     std.debug.print("{X:0^2} ", .{val});
+    // }
+    const msg = circular(2, a);
+    std.debug.print("\n", .{});
+    for (msg) |val| {
+        std.debug.print("{X:0^2} ", .{val});
+    }
     // std.debug.print("{}\n", .{@typeInfo(Block)});
 }
