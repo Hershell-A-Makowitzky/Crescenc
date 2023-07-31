9ff93a09b0a2d475fe0deda071b648eb73918298
Hershell A. Makowitzky <hershellmakowitzky@Abulafia-Air.local>
2023-07-31 21:20:26 +0200

    finale?
    Signed-off-by: Hershell A. Makowitzky <hershell@abulafia.eu>

--- sha.zig
+++ sha.zig
@@ -5,12 +5,20 @@ const K2: u32 = 0x6ED9EBA1;
 const K3: u32 = 0x8F1BBCDC;
 const K4: u32 = 0xCA62C1D6;
 
+var bufferB: [5]u32 = [_]u32{
+    0x67452301,
+    0xEFCDAB89,
+    0x98BADCFE,
+    0x10325476,
+    0xC3D2E1F0,
+};
+
 fn circular(n: u5, w: u32) u32 {
     return (w << n) | (w >> ((31 - n) + 1));
 }
 
-fn f(u: usize, b: u32, c: u32, d: u32) !u32 {
-    if (u <= 19 ) {
+fn f(u: usize, b: u32, c: u32, d: u32) u32 {
+    if (u <= 19) {
         return (b & c) | ((~b) & d);
     }
     if (u <= 39) {
@@ -19,13 +27,10 @@ fn f(u: usize, b: u32, c: u32, d: u32) !u32 {
     if (u <= 59) {
         return (b & c) | (b & d) | (c & d);
     }
-    if (u <= 79) {
-        return b ^ c ^ d;
-    }
-    return error.WordError;
+    return b ^ c ^ d;
 }
 
-fn k(u: usize) !u32 {
+fn k(u: usize) u32 {
     if (u <= 19) {
         return K1;
     }
@@ -35,34 +40,93 @@ fn k(u: usize) !u32 {
     if (u <= 59) {
         return K3;
     }
-    if (u <= 79) {
-        return K4;
-    }
-    return error.WordError;
+    return K4;
 }
 
-fn padding(message: [] const u8) [16]u32 {
-        var length: u64 = message.len;
-        const p_length: *[8]u8 = @ptrCast(*[8]u8, &length);
-        var padded: [16]u32 = undefined;
-        const p_padded: *[64]u8 = @ptrCast(*[64]u8, &padded);
-        for ([_]u8{0}**56) |_, i| {
-            if (i < length) {
-                p_padded[i] = message[i];
-                continue;
-            }
-            if (i == length) {
-                p_padded[i] = '\x80';
-                continue;
-            }
-            p_padded[i] = '\x00';
+fn paddingShort(message: []const u8, length: usize, tail: usize) [16]u32 {
+    var innerLength = length;
+    var p_length: *[8]u8 = @as(*[8]u8, @ptrCast(&innerLength));
+    var padded: [16]u32 = undefined;
+    const p_padded: *[64]u8 = @as(*[64]u8, @ptrCast(&padded));
+    for (0..56, 0..) |_, i| {
+        if (i < tail) {
+            p_padded[i] = message[i];
+            continue;
         }
+        if (i == tail) {
+            p_padded[i] = '\x80';
+            continue;
+        }
+        p_padded[i] = '\x00';
+    }
 
-        length *= 8;
-        for ([_]u8{0}**8) |_, i| {
-            p_padded[56 + i] = p_length[7 - i];
+    innerLength *= 8;
+    for (0..8, 0..) |_, i| {
+        p_padded[56 + i] = p_length[7 - i];
+    }
+    return padded;
+}
+
+fn padding(message: []const u8) [16]u32 {
+    var padded: [16]u32 = undefined;
+    const p_padded: *[64]u8 = @as(*[64]u8, @ptrCast(&padded));
+    for (0..64, 0..) |_, i| {
+        p_padded[i] = message[i];
+    }
+    return padded;
+}
+
+fn paddingSpecial(message: []const u8, tail: usize) [16]u32 {
+    var padded: [16]u32 = undefined;
+    const p_padded: *[64]u8 = @as(*[64]u8, @ptrCast(&padded));
+    for (0..64, 0..) |_, i| {
+        if (i < tail) {
+            p_padded[i] = message[i];
+            continue;
         }
-        return padded;
+        if (i == tail) {
+            p_padded[i] = '\x80';
+            continue;
+        }
+        p_padded[i] = '\x00';
+    }
+    return padded;
+}
+
+fn paddingEnd(length: usize) [16]u32 {
+    var innerLength = length;
+    var p_length: *[8]u8 = @as(*[8]u8, @ptrCast(&innerLength));
+    var padded: [16]u32 = undefined;
+    const p_padded: *[64]u8 = @as(*[64]u8, @ptrCast(&padded));
+    for (0..56, 0..) |_, i| {
+        p_padded[i] = '\x00';
+    }
+
+    innerLength *= 8;
+    for (0..8, 0..) |_, i| {
+        p_padded[56 + i] = p_length[7 - i];
+    }
+    return padded;
+}
+
+fn paddingEndSpecial(length: usize) [16]u32 {
+    var innerLength = length;
+    var p_length: *[8]u8 = @as(*[8]u8, @ptrCast(&innerLength));
+    var padded: [16]u32 = undefined;
+    const p_padded: *[64]u8 = @as(*[64]u8, @ptrCast(&padded));
+    for (0..56, 0..) |_, i| {
+        if (i == 0) {
+            p_padded[i] = '\x80';
+            continue;
+        }
+        p_padded[i] = '\x00';
+    }
+
+    innerLength *= 8;
+    for (0..8, 0..) |_, i| {
+        p_padded[56 + i] = p_length[7 - i];
+    }
+    return padded;
 }
 
 test "k" {
@@ -76,33 +140,38 @@ test "k" {
     std.debug.assert(std.mem.eql(u8, &K4, &k4));
 }
 
-fn calculateHash(message: [] const u8) ![5]u32 {
-    var block: [16]u32 = padding(message);
+fn messageToBLock(message: []const u8) [16]u32 {
+    var block: [16]u32 = undefined;
+    const p_padded: *[64]u8 = @as(*[64]u8, @ptrCast(&block));
+    for (0..64, 0..) |_, i| {
+        p_padded[i] = message[i];
+    }
+    return block;
+}
+
+fn calculateHash(message: *[16] u32) [5]u32 {
+    // var block: [16]u32 = messageToBLock(message);
+    // var block: *[16]u32 = @ptrCast(&message);
     var bufferA: [5]u32 = undefined;
-    var bufferB: [5]u32 = [_]u32{
-        0x67452301,
-        0xEFCDAB89,
-        0x98BADCFE,
-        0x10325476,
-        0xC3D2E1F0,
-    };
     var temp: u32 = undefined;
     var seq: [80]u32 = undefined;
 
-    for (seq) |*val, i| {
+    // printPadded(message);
+
+    for (&seq, 0..) |*val, i| {
         if (i < 16) {
-            val.* = std.mem.nativeToBig(u32, block[i]);
+            val.* = std.mem.nativeToBig(u32, message[i]);
         } else {
             val.* = circular(1, seq[i - 3] ^ seq[i - 8] ^ seq[i - 14] ^ seq[i - 16]);
         }
     }
 
-    for (bufferA) |*val, i| {
+    for (&bufferA, 0..) |*val, i| {
         val.* = bufferB[i];
     }
 
-    for ([_]u8{0} ** 80) |_, i| {
-        temp = circular(5, bufferA[0]) +% try f(i, bufferA[1], bufferA[2], bufferA[3]) +% bufferA[4] +% seq[i] +% try k(i);
+    for ([_]u8{0} ** 80, 0..) |_, i| {
+        temp = circular(5, bufferA[0]) +% f(i, bufferA[1], bufferA[2], bufferA[3]) +% bufferA[4] +% seq[i] +% k(i);
         bufferA[4] = bufferA[3];
         bufferA[3] = bufferA[2];
         bufferA[2] = circular(30, bufferA[1]);
@@ -121,28 +190,106 @@ fn calculateHash(message: [] const u8) ![5]u32 {
 
 fn printHash(hash: [5]u32) void {
     for (hash) |val| {
-        std.debug.print("{x:0^8}", .{val});
+        std.debug.print("{x:0>8}", .{val});
     }
     std.debug.print("\n", .{});
 }
 
-fn splitMessage(message: [] const u8) !void {
+fn printPadded(block: [16]u32) void {
+    for (block, 0..) |value, i| {
+        if (i % 4 == 0) {
+            std.debug.print("\n", .{});
+        }
+        std.debug.print("{x:0>8} ", .{std.mem.nativeToBig(u32, value)});
+    }
+    std.debug.print("\n\n", .{});
+}
+
+fn splitMessage(message: []const u8) !void {
     var start: usize = 0;
     var stop: usize = 64;
     var length = message.len;
-    while (length > 64) {
-        // std.debug.print("{s}\n", .{message[start..stop]});
-        const result = try calculateHash(message[start..stop]);
-        printHash(result);
-        start = stop;
-        stop += 64;
-        length -= 64;
-    }
-    const rest: usize = start + length;
-    // std.debug.print("{s}\n", .{message[start..rest]});
-    const result = try calculateHash(message[start..rest]);
-    printHash(result);
+    var tail = length;
+    if (length >= 56) {
+        while (tail > 64) {
+            // if (tail == 63) {
+            //     var rest = padding(message[start..stop]);
+            //     // std.debug.print("{s}\n", .{message[start..stop]});
+            //     const result = calculateHash(&rest);
+            //     printPadded(rest);
+            //     printHash(result);
+            //     return;
+            // }
+            var rest = padding(message[start..stop]);
+            // std.debug.print("{s}\n", .{message[start..stop]});
+            _ = calculateHash(&rest);
+            // printPadded(rest);
+            start = stop;
+            stop += 64;
+            tail -= 64;
+        }
+        // if (tail == 63) {
+        //     var rest = padding(message[start..stop]);
+        //     // std.debug.print("{s}\n", .{message[start..stop]});
+        //     const result = calculateHash(&rest);
+        //     printPadded(rest);
+        //     printHash(result);
+        //     return;
+        // }
+        // if (tail % 64 == 0) {
+        //     stop = tail;
+        //     std.debug.print("KVA", .{});
+        //     var rest = paddingSpecial(message[start..stop], tail);
+        //     printPadded(rest);
+        //     _ = calculateHash(&rest);
 
+        //     rest = paddingEndSpecial(length);
+        //     printPadded(rest);
+        //     const result = calculateHash(&rest);
+        //     printHash(result);
+        //     return;
+        // }
+        if (tail >= 56 and tail < 64) {
+            // std.debug.print("56 and 64\n", .{});
+            stop = tail;
+            var rest = paddingSpecial(message[start..stop], tail);
+            // printPadded(rest);
+            _ = calculateHash(&rest);
+            rest = paddingEnd(length);
+            // printPadded(rest);
+            const result = calculateHash(&rest);
+            printHash(result);
+            return;
+            // printHash(result);
+        } else if (tail == 64) {
+            // std.debug.print("64\n", .{});
+            stop = tail;
+            var rest = padding(message[start..stop]);
+            // printPadded(rest);
+            _ = calculateHash(&rest);
+            rest = paddingEndSpecial(length);
+            // printPadded(rest);
+            const result = calculateHash(&rest);
+            printHash(result);
+            return;
+        }
+        // std.debug.print("rest\n", .{});
+        var rest = paddingShort(message[start..stop], length, tail);
+        // printPadded(rest);
+        const result = calculateHash(&rest);
+        printHash(result);
+        return;
+    }
+
+    var rest = paddingShort(message, length, length);
+    // std.debug.print("IN SHORT\n", .{});
+    // printPadded(rest);
+    const result = calculateHash(&rest);
+    printHash(result);
+    // const rest: usize = start + length;
+    // std.debug.print("{s}\n", .{message[start..rest]});
+    // const result = calculateHash(message[start..rest]);
+    // printHash(res&ult);
 }
 
 pub fn main() !void {
@@ -156,6 +303,7 @@ pub fn main() !void {
         std.process.exit(1);
     }
     const message = args[1];
+    // std.debug.print("{s}\n", .{message});
     // const result = try calculateHash(message);
     // printHash(result);
     _ = try splitMessage(message);
