e9daa2a11e99cfe7c20170e8a5d385ba3c6d8f52
Hershell A. Makowitzky <hershellmakowitzky@Abulafia-MacBook.local>
2023-08-06 23:13:11 +0200

    remove comments & tests
    Signed-off-by: Hershell A. Makowitzky <hershell@abulafia.eu>

--- sha.zig
+++ sha.zig
@@ -130,17 +130,6 @@ fn paddingEndSpecial(length: usize) [16]u32 {
     return padded;
 }
 
-test "k" {
-    const k1 = try k(1);
-    const k2 = try k(21);
-    const k3 = try k(41);
-    const k4 = try k(61);
-    std.debug.assert(std.mem.eql(u8, &K1, &k1));
-    std.debug.assert(std.mem.eql(u8, &K2, &k2));
-    std.debug.assert(std.mem.eql(u8, &K3, &k3));
-    std.debug.assert(std.mem.eql(u8, &K4, &k4));
-}
-
 fn messageToBLock(message: []const u8) [16]u32 {
     var block: [16]u32 = undefined;
     const p_padded: *[64]u8 = @as(*[64]u8, @ptrCast(&block));
@@ -151,13 +140,10 @@ fn messageToBLock(message: []const u8) [16]u32 {
 }
 
 fn calculateHash(message: *[16]u32) [5]u32 {
-    // var block: [16]u32 = messageToBLock(message);
-    // var block: *[16]u32 = @ptrCast(&message);
     var bufferA: [5]u32 = undefined;
     var temp: u32 = undefined;
     var seq: [80]u32 = undefined;
 
-    // printPadded(message);
 
     for (&seq, 0..) |*val, i| {
         if (i < 16) {
@@ -193,7 +179,6 @@ fn printHash(hash: [5]u32) void {
     for (hash) |val| {
         std.debug.print("{x:0>8}", .{val});
     }
-    // std.debug.print("\n", .{});
 }
 
 fn printPadded(block: [16]u32) void {
@@ -206,179 +191,47 @@ fn printPadded(block: [16]u32) void {
     std.debug.print("\n\n", .{});
 }
 
-fn splitMessage(message: []const u8) !void {
-    var start: usize = 0;
-    var stop: usize = 64;
-    var length = message.len;
-    var tail = length;
-    if (length >= 56) {
-        while (tail > 64) {
-            // if (tail == 63) {
-            //     var rest = padding(message[start..stop]);
-            //     // std.debug.print("{s}\n", .{message[start..stop]});
-            //     const result = calculateHash(&rest);
-            //     printPadded(rest);
-            //     printHash(result);
-            //     return;
-            // }
-            var rest = padding(message[start..stop]);
-            // std.debug.print("{s}\n", .{message[start..stop]});
-            _ = calculateHash(&rest);
-            // printPadded(rest);
-            start = stop;
-            stop += 64;
-            tail -= 64;
-        }
-        // if (tail == 63) {
-        //     var rest = padding(message[start..stop]);
-        //     // std.debug.print("{s}\n", .{message[start..stop]});
-        //     const result = calculateHash(&rest);
-        //     printPadded(rest);
-        //     printHash(result);
-        //     return;
-        // }
-        // if (tail % 64 == 0) {
-        //     stop = tail;
-        //     std.debug.print("KVA", .{});
-        //     var rest = paddingSpecial(message[start..stop], tail);
-        //     printPadded(rest);
-        //     _ = calculateHash(&rest);
-
-        //     rest = paddingEndSpecial(length);
-        //     printPadded(rest);
-        //     const result = calculateHash(&rest);
-        //     printHash(result);
-        //     return;
-        // }
-        if (tail >= 56 and tail < 64) {
-            // std.debug.print("56 and 64\n", .{});
-            stop = tail;
-            var rest = paddingSpecial(message[start..stop], tail);
-            // printPadded(rest);
-            _ = calculateHash(&rest);
-            rest = paddingEnd(length);
-            // printPadded(rest);
-            const result = calculateHash(&rest);
-            printHash(result);
-            return;
-            // printHash(result);
-        } else if (tail == 64) {
-            // std.debug.print("64\n", .{});
-            stop = tail;
-            var rest = padding(message[start..stop]);
-            // printPadded(rest);
-            _ = calculateHash(&rest);
-            rest = paddingEndSpecial(length);
-            // printPadded(rest);
-            const result = calculateHash(&rest);
-            printHash(result);
-            return;
-        }
-        // std.debug.print("rest\n", .{});
-        var rest = paddingShort(message[start..stop], length, tail);
-        // printPadded(rest);
-        const result = calculateHash(&rest);
-        printHash(result);
-        return;
-    }
-
-    var rest = paddingShort(message, length, length);
-    // std.debug.print("IN SHORT\n", .{});
-    // printPadded(rest);
-    const result = calculateHash(&rest);
-    printHash(result);
-    // const rest: usize = start + length;
-    // std.debug.print("{s}\n", .{message[start..rest]});
-    // const result = calculateHash(message[start..rest]);
-    // printHash(res&ult);
-}
-
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
-    // if (args.len != 2) {
-    //     std.debug.print("Usage '{s}' <string>\n", .{args[0]});
-    //     std.process.exit(1);
-    // }
-    // const message = args[1];
     if (args.len == 1 or std.mem.eql(u8, args[1], "-")) {
-        // std.debug.print("{}\n", .{args.len});
-        // std.debug.print("{}\n", .{std.mem.eql(u8, message, "-")});
-        // std.debug.print("ARGS: {s}\n", .{message});
         const stdin = std.io.getStdIn();
         var buffer: [64]u8 = undefined;
         var index = try stdin.read(&buffer);
         var strlen: usize = undefined;
         while (index == 64) {
-            // std.debug.print("In while loop\n", .{});
             strlen += index;
             var rest = padding(buffer[0..index]);
-            // printPadded(rest);
             _ = calculateHash(&rest);
             index = try stdin.read(&buffer);
         }
         switch (index) {
             0 => {
-                // std.debug.print("Out of while loop\n", .{});
                 var rest = paddingEndSpecial(strlen);
-                // printPadded(rest);
                 const result = calculateHash(&rest);
                 printHash(result);
             },
             1...55 => {
-                // std.debug.print("In lower 56\n", .{});
                 strlen += index;
                 var rest = paddingShort(buffer[0..index], strlen, index);
-                // printPadded(rest);
                 const result = calculateHash(&rest);
                 printHash(result);
             },
             56...63 => {
-                // std.debug.print("In between 56 and 64\n", .{});
-                // std.debug.print("56 and 64\n", .{});
                 strlen += index;
                 var rest = paddingSpecial(buffer[0..index], index);
-                // printPadded(rest);
                 _ = calculateHash(&rest);
                 rest = paddingEnd(strlen);
-                // printPadded(rest);
                 const result = calculateHash(&rest);
                 printHash(result);
             },
             else => {}
         }
-        // if (index == 0) {
-        //     std.debug.print("Out of while loop\n", .{});
-        //     var rest = paddingEndSpecial(strlen);
-        //     printPadded(rest);
-        //     const result = calculateHash(&rest);
-        //     printHash(result);
-        // }
-        // if (index < 56) {
-        //     std.debug.print("In lower 56\n", .{});
-        //     var rest = paddingSpecial(buffer[0..index], index);
-        //     const result = calculateHash(&rest);
-        //     printHash(result);
-        // }
-        // if (index >= 56 and index < 64) {
-        //     std.debug.print("In between 56 and 64\n", .{});
-        //     // std.debug.print("56 and 64\n", .{});
-        //     var rest = paddingSpecial(buffer[0..index], index);
-        //     // printPadded(rest);
-        //     _ = calculateHash(&rest);
-        //     rest = paddingEnd(strlen);
-        //     // printPadded(rest);
-        //     const result = calculateHash(&rest);
-        //     printHash(result);
-        // }
         std.debug.print("  -\n", .{});
     } else {
         std.debug.print("IN ELSE BRANCH\n", .{});
     }
-    // std.debug.print("{s}\n", .{message});
-    // const result = try calculateHash(message);
-    // printHash(result);
 }
